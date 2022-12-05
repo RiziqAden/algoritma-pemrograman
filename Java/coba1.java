549da1b6ed2b1e8c64b422973fd8b512dcf861ca
RiziqAden <riziq.unida@gmail.com>
2022-12-05 14:24:21 +0700

    m
    latihan

---
+++ Java/coba1.java
@@ -0,0 +1,12 @@
+class coba1 {
+    public static void main(String[] args) {
+    String nim = "I.2211266";
+    String nama = "M. Riziq Sirfatullah Alfarizi";
+    String umur = "18";
+    String profesi = "mahasiswa";
+    System.out.println(nim);
+    System.out.println(nama);
+    System.out.println(umur);
+    System.out.println(profesi);
+    }
+   }
